917490ded22ef8281b84ec8fc6d251a8d221fba3
AHMEDKISHK7 <a.kishk9437@gmail.com>
2023-04-05 17:41:14 -0700

    You must not be afraid to dream

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,11 +9,17 @@ int factorial(int n)
 	int a;
 
 	if (n == 0)
-		return (1);}
+	{
+		return (1);
+	}
 	else if (n < 0)
+	{
 		return (-1);
+	}
 	else
+	{
 		x = n * factorial(n - 1);
+	}
 		return (a);
 
 }
