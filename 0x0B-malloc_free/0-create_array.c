4a9daef2686123df4bb142adac37788cc68bb11b
AHMEDKISHK7 <a.kishk9437@gmail.com>
2023-04-11 09:15:34 -0700

    Float like a butterfly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,11 +8,12 @@
  * Description: creat array of size size and assign char c		
  * Return: pointer to array, NULL if fail		
  */
+char *create_array(unsigned int size, char c)
 
-char *create_array(unsigned int size, char c)		
 {		
 	char *str;		
 	unsigned int i;				
+
 	str = malloc(sizeof(char) * size);		
 	if (size == 0 || str == NULL)		
 		return (NULL);
